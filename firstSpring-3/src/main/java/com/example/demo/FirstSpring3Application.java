01dcb6ebd61cc8b646be08072072223fbb2aebc8
yokouchiRyouta <ryoutako0901@gmail.com>
2018-08-28 14:20:31 +0900

    this is first commit

---
+++ firstSpring-3/src/main/java/com/example/demo/FirstSpring3Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpring3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpring3Application.class, args);
+	}
+}
